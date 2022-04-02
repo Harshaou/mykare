5556640f3b8f327bf2923da87b4725f526f611b3
Harshad <harsh.haou@gmail.com>
2022-04-02 14:13:55 +0530

    improvememts

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import { PrivateRoute } from "./PrivateRoute";
 import Signup from "../pages/RegisterPage";
 import Login from "../pages/LoginPage";
 import Dashboard from "../pages/DashboardPage";
+import NotFound from "../pages/404.js";
 
 const RouterSetup = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<NotFound />} />
       <Route
         path="/"
         element={
